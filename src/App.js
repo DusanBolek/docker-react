ba5d1649bf49a172a2f6a77cd6204d1aca15bd67
Dusan Bolek <dusanbolek@yahoo.com>
2021-04-26 15:17:36 +0200

    changed app text including test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          do not learn react
         </a>
       </header>
     </div>
